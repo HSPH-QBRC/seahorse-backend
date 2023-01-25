6ffe1a1df431c1bac319c7d6d356d9dab0e7cce1
Ilya Sytchev <isytchev@hsph.harvard.edu>
2023-01-25 17:52:19 -0500

    Code formatting

--- import.sql
+++ import.sql
@@ -1,7 +1,8 @@
 select aws_commons.create_s3_uri(
     'seahorse-data',
     'db_tables/geneexpression_data.subset_1per.tsv.gz',
-    'us-east-2') as s3_uri \gset
+    'us-east-2'
+) as s3_uri \gset
 
 select aws_s3.table_import_from_s3(
     'expression',
