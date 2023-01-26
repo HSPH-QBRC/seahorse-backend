da51d3a5b6d5cf99faa37f19a75a258b95447d3c
Ilya Sytchev <isytchev@hsph.harvard.edu>
2023-01-26 16:59:40 -0500

    Add definition for annotations table

--- import.sql
+++ import.sql
@@ -1,11 +1,10 @@
 select aws_commons.create_s3_uri(
     'seahorse-data',
-    'db_tables/geneexpression_data.tsv.gz',
+    'db_tables/metadata_data.tsv.gz',
     'us-east-2'
 ) as s3_uri \gset
-
 select aws_s3.table_import_from_s3(
-    'expression',
+    'annotations',
     '',
     '(format csv, delimiter E''\t'', header)',
     :'s3_uri'
@@ -16,9 +15,21 @@ select aws_commons.create_s3_uri(
     'db_tables/human_ensembl2symbol_map.tsv.gz',
     'us-east-2'
 ) as s3_uri \gset
-
 select aws_s3.table_import_from_s3(
     'ensembl2symbol',
+    '',
+    '(format csv, delimiter E''\t'', header)',
+    :'s3_uri'
+);
+
+select aws_commons.create_s3_uri(
+    'seahorse-data',
+    'db_tables/geneexpression_data.tsv.gz',
+    'us-east-2'
+) as s3_uri \gset
+
+select aws_s3.table_import_from_s3(
+    'expression',
     '',
     '(format csv, delimiter E''\t'', header)',
     :'s3_uri'
--- init.sql
+++ init.sql
@@ -1,8 +1,71 @@
-create table gene_expression
+create table annotations
 (
-    ensembl_id varchar(22),
-    gtex_id    varchar(30),
-    level      integer
+    gtex_id   varchar(42),
+    smatsscr  real,
+    smcenter  varchar(10),
+    smpthnts  text,
+    smrin     real,
+    smts      varchar(20),
+    smtsd     varchar(45),
+    smubrid   varchar(12),
+    smtsisch  real,
+    smtspax   real,
+    smnabtch  varchar(40),
+    smnabtcht varchar(55),
+    smnabtchd date,
+    smgebtch  varchar(35),
+    smgebtchd date,
+    smgebtcht varchar(40),
+    smafrze   varchar(10),
+    smgtc     varchar(20),
+    sme2mprt  real,
+    smchmprs  real,
+    smntrart  real,
+    smnumgps  integer,
+    smmaprt   real,
+    smexncrt  real,
+    sm550nrm  integer,
+    smgnsdtc  real,
+    smunmprt  real,
+    sm350nrm  integer,
+    smrdlgth  real,
+    smmncpb   integer,
+    sme1mmrt  real,
+    smsflgth  real,
+    smestlbs  real,
+    smmppd    real,
+    smnterrt  real,
+    smrrnanm  real,
+    smrdttl   real,
+    smvqcfl   real,
+    smmncv    integer,
+    smtrscpt  real,
+    smmppdpr  real,
+    smcglgth  integer,
+    smgappct  integer,
+    smunpdrd  real,
+    smntrnrt  real,
+    smmpunrt  real,
+    smexpeff  real,
+    smmppdun  real,
+    sme2mmrt  real,
+    sme2anti  real,
+    smaltalg  real,
+    sme2snse  real,
+    smmflgth  real,
+    sme1anti  real,
+    smspltrd  real,
+    smbsmmrt  real,
+    sme1snse  real,
+    sme1pcts  real,
+    smrrnart  real,
+    sme1mprt  real,
+    smnum5cd  integer,
+    smdpmprt  real,
+    sme2pcts  real,
+    sex       integer,
+    age       varchar(5),
+    dthhrdy   real
 );
 
 create table ensembl2symbol
@@ -11,4 +74,11 @@ create table ensembl2symbol
     ensembl_id varchar(22),
     symbol     varchar(25),
     entrez_id  integer
+);
+
+create table gene_expression
+(
+    ensembl_id varchar(22),
+    gtex_id    varchar(30),
+    level      integer
 );
