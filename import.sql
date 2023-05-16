33bd23e3ad37422e60fd3416127c09618f87bec8
Ilya Sytchev <isytchev@hsph.harvard.edu>
2023-05-16 13:59:22 -0400

    Remove annotations table

--- import.sql
+++ import.sql
@@ -1,17 +1,5 @@
 select aws_commons.create_s3_uri(
     'seahorse-data',
-    'db_tables/metadata_data.tsv.gz',
-    'us-east-2'
-) as s3_uri \gset
-select aws_s3.table_import_from_s3(
-    'annotations',
-    '',
-    '(format csv, delimiter E''\t'', header)',
-    :'s3_uri'
-);
-
-select aws_commons.create_s3_uri(
-    'seahorse-data',
     'db_tables/data_dictionary.tsv.gz',
     'us-east-2'
 ) as s3_uri \gset
